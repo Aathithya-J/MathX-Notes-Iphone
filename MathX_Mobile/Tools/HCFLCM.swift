07d3a9cb97e16ead6318c3f882fac263263706b0
cheesetan <93832702+cheesetan@users.noreply.github.com>
2023-04-21 23:28:07 +0800

    added old lcm view back to hcflcmview, forgot to add it back before

--- MathX_Mobile/Tools/HCFLCM.swift
+++ MathX_Mobile/Tools/HCFLCM.swift
@@ -1,12 +1,15 @@
 import SwiftUI
 
 struct HCF_LCM_CalculatorView: View  {
+    
     @State var position: CGRect = .zero
     
     @State var lhsNumber: Int
     @State var rhsNumber: Int
     
     @State var showOnlyPrime: Bool = false
+    
+    @State var HCFLCMSelection = 0
     
     var lhsFactors: [Int] { factors(of: lhsNumber) }
     var rhsFactors: [Int] { factors(of: rhsNumber) }
@@ -42,46 +45,64 @@ struct HCF_LCM_CalculatorView: View  {
     @State var trailingStartOffset: CGFloat?
     
     var body: some View {
-        List {
-//            Text("""
-//                minX: \(position.minX)
-//                maxX: \(position.maxX)
-//                minY: \(position.minY)
-//                maxY: \(position.maxY)
-//                common: \(commonFactors.description)
-//            """)
-            Section {
-                TextField("First Number", text: .init(get: { lhsNumber.description },
-                                                      set: { lhsNumber = Int($0) ?? 0 }))
-                TextField("Second Number", text: .init(get: { rhsNumber.description },
-                                                       set: { rhsNumber = Int($0) ?? 0 }))
-                Toggle("Only show prime numbers", isOn: .init(get: {
-                    showOnlyPrime
-                }, set: { newValue in
-                    withAnimation {
-                        showOnlyPrime = newValue
-                    }
-                }))
+        VStack {
+            Picker("", selection: $HCFLCMSelection) {
+                Text("HCF")
+                    .tag(0)
+                Text("LCM")
+                    .tag(1)
             }
+            .pickerStyle(.segmented)
+            .padding(.top)
+            .padding(.horizontal)
             
-            Section {
-                HStack(alignment: .top) {
-                    ribbonComponent
-                }
-                .background {
-                    GeometryReader { geom in
-                        Color.clear
-                            .onAppear {
-                                position = geom.frame(in: .named("listCoordinate"))
+            if HCFLCMSelection == 0 {
+                List {
+                    //            Text("""
+                    //                minX: \(position.minX)
+                    //                maxX: \(position.maxX)
+                    //                minY: \(position.minY)
+                    //                maxY: \(position.maxY)
+                    //                common: \(commonFactors.description)
+                    //            """)
+                    
+                    Section {
+                        TextField("First Number", text: .init(get: { lhsNumber.description },
+                                                              set: { lhsNumber = Int($0) ?? 0 }))
+                        TextField("Second Number", text: .init(get: { rhsNumber.description },
+                                                               set: { rhsNumber = Int($0) ?? 0 }))
+                        Toggle("Only show prime numbers", isOn: .init(get: {
+                            showOnlyPrime
+                        }, set: { newValue in
+                            withAnimation {
+                                showOnlyPrime = newValue
                             }
-                            .onChange(of: geom.frame(in: .named("listCoordinate"))) { newValue in
-                                position = newValue
+                        }))
+                    }
+                    
+                    Section {
+                        HStack(alignment: .top) {
+                            ribbonComponent
+                        }
+                        .background {
+                            GeometryReader { geom in
+                                Color.clear
+                                    .onAppear {
+                                        position = geom.frame(in: .named("listCoordinate"))
+                                    }
+                                    .onChange(of: geom.frame(in: .named("listCoordinate"))) { newValue in
+                                        position = newValue
+                                    }
                             }
+                        }
                     }
                 }
+                .coordinateSpace(name: "listCoordinate")
+            } else {
+                OldHCF_LCM_CalculatorView(isHCFSelected: $HCFLCMSelection)
             }
         }
-        .coordinateSpace(name: "listCoordinate")
+        .navigationTitle("HCF & LCM Calculator")
     }
     
     @Namespace var namespace
@@ -223,17 +244,18 @@ struct BezierRectangle: Shape {
     }
 }
 
-//struct HCF_LCM_CalculatorView: View {
-//    @State private var number1 = ""
-//    @State private var number2 = ""
-//    @State private var result = ""
-//    @State private var isHCFSelected = true
-//
-//    var body: some View {
-//        VStack {
-//            Picker(selection: $isHCFSelected, label: Text("Select Operation")) {
-//                Text("HCF").tag(true)
-//                Text("LCM").tag(false)
+struct OldHCF_LCM_CalculatorView: View {
+    
+    @State private var number1 = ""
+    @State private var number2 = ""
+    @State private var result = ""
+    @Binding var isHCFSelected: Int
+
+    var body: some View {
+        VStack {
+//            Picker("", selection: $isHCFSelected) {
+//                Text("HCF").tag(0)
+//                Text("LCM").tag(1)
 //            }
 //            .pickerStyle(SegmentedPickerStyle())
 //            .padding()
@@ -241,71 +263,74 @@ struct BezierRectangle: Shape {
 //            .onChange(of: isHCFSelected) { _ in
 //                self.calculate()
 //            }
-//
-//            VStack {
-//                TextField("Enter the first number", text: $number1)
-//                    .textFieldStyle(.plain)
-//                    .keyboardType(.decimalPad)
-//                    .padding()
-//                    .frame(width: UIScreen.main.bounds.width - 30, height: 50)
-//                    .background(.ultraThickMaterial)
-//                    .cornerRadius(16)
-//
-//                TextField("Enter the second number", text: $number2)
-//                    .textFieldStyle(.plain)
-//                    .keyboardType(.decimalPad)
-//                    .padding()
-//                    .frame(width: UIScreen.main.bounds.width - 30, height: 50)
-//                    .background(.ultraThickMaterial)
-//                    .cornerRadius(16)
-//            }
-//
-//            if !number1.isEmpty && !number2.isEmpty {
-//                Text("Result: \(result)")
-//                    .padding()
-//            }
-//
-//            Spacer()
-//        }
-//        .onChange(of: number1) { _ in
-//            self.calculate()
-//        }
-//        .onChange(of: number2) { _ in
-//            self.calculate()
-//        }
+
+            VStack {
+                TextField("First number", text: $number1)
+                    .textFieldStyle(.plain)
+                    .keyboardType(.decimalPad)
+                    .padding()
+                    .frame(width: UIScreen.main.bounds.width - 30, height: 50)
+                    .background(.ultraThickMaterial)
+                    .cornerRadius(16)
+
+                TextField("Second number", text: $number2)
+                    .textFieldStyle(.plain)
+                    .keyboardType(.decimalPad)
+                    .padding()
+                    .frame(width: UIScreen.main.bounds.width - 30, height: 50)
+                    .background(.ultraThickMaterial)
+                    .cornerRadius(16)
+            }
+            .padding(.top, 30)
+
+            if !number1.isEmpty && !number2.isEmpty {
+                Text("LCM: \(result)")
+                    .padding()
+                    .background(.ultraThickMaterial)
+                    .cornerRadius(16)
+            }
+
+            Spacer()
+        }
+        .onChange(of: number1) { _ in
+            self.calculate()
+        }
+        .onChange(of: number2) { _ in
+            self.calculate()
+        }
 //        .navigationTitle("HCF & LCM Calculator")
-//        .onAppear {
-//            self.calculate()
-//        }
-//    }
-//
-//    func calculate() {
-//        guard let num1 = Double(number1), let num2 = Double(number2) else {
-//            self.result = "Please enter valid numbers"
-//            return
-//        }
-//
-//        let gcd = findGCD(Int(num1 * 100), Int(num2 * 100))
-//        let lcm = Int(num1 * 100 * num2 * 100) / gcd
-//
-//        if isHCFSelected {
-//            self.result = String((Double(gcd) / 100).formatted())
-//        } else {
-//            self.result = String((Double(lcm) / 100).formatted())
-//        }
-//    }
-//
-//    func findGCD(_ num1: Int, _ num2: Int) -> Int {
-//        if num2 == 0 {
-//            return num1
-//        } else {
-//            return findGCD(num2, num1 % num2)
-//        }
-//    }
-//}
-//
-//struct HCF_LCM_CalculatorView_Previews: PreviewProvider {
+        .onAppear {
+            self.calculate()
+        }
+    }
+
+    func calculate() {
+        guard let num1 = Double(number1), let num2 = Double(number2) else {
+            self.result = ""
+            return
+        }
+
+        let gcd = findGCD(Int(num1 * 100), Int(num2 * 100))
+        let lcm = Int(num1 * 100 * num2 * 100) / gcd
+
+        if isHCFSelected == 0  {
+            self.result = String((Double(gcd) / 100).formatted())
+        } else {
+            self.result = String((Double(lcm) / 100).formatted())
+        }
+    }
+
+    func findGCD(_ num1: Int, _ num2: Int) -> Int {
+        if num2 == 0 {
+            return num1
+        } else {
+            return findGCD(num2, num1 % num2)
+        }
+    }
+}
+
+//struct OldHCF_LCM_CalculatorView_Previews: PreviewProvider {
 //    static var previews: some View {
-//        HCF_LCM_CalculatorView()
+//        OldHCF_LCM_CalculatorView()
 //    }
 //}
